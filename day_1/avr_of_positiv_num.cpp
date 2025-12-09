5040e466a4579dc25cae09e35be6829183fba442
vaghelaharsh1015 <harshvaghela4277@gmail.com>
2025-12-09 13:58:32 +0530

    new file

--- day_1/avr_of_positiv_num.cpp
+++ day_1/avr_of_positiv_num.cpp
@@ -2,28 +2,31 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
 
-    int arr[]={-1,2,-3,-4,5,6,-7,8,9,10};
+    int arr[] = {-1, 2, -3, -4, 5, 6, -7, 8, 9, 100};
 
-    int size=sizeof(arr)/sizeof(arr[0]);
+    int size = sizeof(arr) / sizeof(arr[0]);
 
-    int positive_sum=0;
+    int positive_sum = 0;
 
-    int positivecount=0;
+    int positivecount = 0;
 
-    float avr=0;
+    float avr = 0;
 
-    for(int i=0;i<size;i++){
-        if(arr[i]>0){
-            positive_sum+=arr[i];
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] > 0)
+        {
+            positive_sum += arr[i];
             positivecount++;
         }
     }
 
-    avr=positive_sum/positivecount;
+    avr = positive_sum / positivecount;
 
-    cout<<"positive sum avcr is : "<<avr<<endl;
+    cout << "positive sum ave is : " << avr << endl;
 
     return 0;
 }
