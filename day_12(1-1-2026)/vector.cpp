bc649e847f6a2d2f21e2cf5525d62f6c95bd07fa
vaghelaharsh1015 <harshvaghela4277@gmail.com>
2026-01-01 15:54:39 +0530

    new file

--- day_12(1-1-2026)/vector.cpp
+++ day_12(1-1-2026)/vector.cpp
@@ -1,18 +1,22 @@
-#include <iostream>
-#include <vector>
+#include<iostream>
+#include<vector>
+
 using namespace std;
 
-int main() {
+int mian(){
+    
+    vector<int> v={ 10 , 15 , 20 , 30 , 40 , 50 };
 
-    vector<int> v;
+    int pos = 2;
 
-    v.push_back(10);
-    v.push_back(40);
-    v.push_back(30);
+    int value = 25;
 
-    cout << "Vector elements: ";
-    for(int i = 0; i < v.size(); i++){
-        cout << v[i] << " ";
+    v.insert(v.begin() + pos , value);
+
+    for(int  y : v)
+    {
+        cout << y << "";
     }
     return 0;
+
 }
