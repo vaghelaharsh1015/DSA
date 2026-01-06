f9d0ad34a763ed598679e76efdf09685aebdbf26
vaghelaharsh1015 <harshvaghela4277@gmail.com>
2026-01-06 15:35:31 +0530

    new file

---
+++ day_14(6-1-26)/linked_list.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+class Node{
+  public:
+  int data;
+  Node* next;
+
+  Node(int val)
+  {
+    data = val;
+    next = null;
+  }
+};
+
+// insert at the end
+
+void insert(Node* &head , int val)
+{
+  
+}
